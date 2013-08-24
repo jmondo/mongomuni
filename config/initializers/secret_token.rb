66f5f7cacd60232fa24684dc01416263fd89292a
John Gesimondo <john@jmondo.com>
2013-08-23 22:46:31 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Munimongo3::Application.config.secret_token = 'e2a5cdbfce7efecfc112010a37b58554bf52b2437631711826372fb0f8b54cefeede420b36ec117f20fe572999a15fe7093b5cd6e2cba3d3b1a394493b9aa31a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Munimongo3::Application.config.session_store :cookie_store, key: '_munimongo3_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Munimongo3::Application.config.session_store :active_record_store
