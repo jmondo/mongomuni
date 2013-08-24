da29e7a12445194f48071eabd969db007795b961
John Gesimondo <john@jmondo.com>
2013-08-24 14:27:35 -0700

    fallback

--- app/controllers/vehicle_location_scrapes_controller.rb
+++ app/controllers/vehicle_location_scrapes_controller.rb
@@ -1,6 +1,6 @@
 class VehicleLocationScrapesController < ApplicationController
   def create
-    route_ids = params[:route_ids]
+    route_ids = params[:route_ids] || []
     route_ids.each do |route|
       VehicleLocationScraper.new(route).create_vehicle_locations
     end
