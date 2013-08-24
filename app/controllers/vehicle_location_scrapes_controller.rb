8f489d33baef11dd6519c40c1a8ae4c10b5a57e2
John Gesimondo <john@jmondo.com>
2013-08-24 14:12:06 -0700

    response

--- app/controllers/vehicle_location_scrapes_controller.rb
+++ app/controllers/vehicle_location_scrapes_controller.rb
@@ -4,5 +4,6 @@ class VehicleLocationScrapesController < ApplicationController
     route_ids.each do |route|
       VehicleLocationScraper.new(route).create_vehicle_locations
     end
+    render text: "Successfully retrieved data for routes #{route_ids.join(', ')}"
   end
 end
