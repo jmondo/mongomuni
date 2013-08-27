287e7610d751a2a9a28f02f69333489c9b46dedc
John Gesimondo <john@jmondo.com>
2013-08-26 22:20:14 -0700

    use floats… doh

--- app/models/vehicle_location.rb
+++ app/models/vehicle_location.rb
@@ -3,9 +3,14 @@ class VehicleLocation
   include Mongoid::Timestamps
 
   field :location, type: Array
+  field :lat, type: Float
+  field :lon, type: Float
+
+  index({ location: "2d" }, { min: -180, max: 180 })
   index({ location: "2dsphere" }, { min: -180, max: 180 })
 
   set_callback(:validation, :before) do |document|
-    document.location = [document.lat, document.lon]
+    document.location = [document.lat.to_f, document.lon.to_f]
+  end
   end
 end
