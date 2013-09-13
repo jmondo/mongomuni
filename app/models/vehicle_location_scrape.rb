d5344c84e2e547bca39abbca0c6b2f3e3543ba64
John Gesimondo <john@jmondo.com>
2013-09-12 20:27:55 -0700

    add column types

--- app/models/vehicle_location_scrape.rb
+++ app/models/vehicle_location_scrape.rb
@@ -1,4 +1,7 @@
 # a class just for recording when the last scrape was (because the api needs to know)
 class VehicleLocationScrape
   include Mongoid::Document
+
+  field :time, type: DateTime
+  field :routeTag, type: String
 end
